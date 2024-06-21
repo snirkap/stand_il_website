cc8542ad540a2ddc910be52e9e5394160bbfce45
snirkap <snirwork1@gmail.com>
2024-06-21 12:58:48 +0300

    delete s3 force destroy

--- tf/modules/s3_client_info/s3.tf
+++ tf/modules/s3_client_info/s3.tf
@@ -2,7 +2,6 @@ module "s3_bucket"{
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = var.s3_bucket_name
   acl    = "private"
-  force_destroy = true
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
   versioning = {
